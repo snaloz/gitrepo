bfef0a3488131355201b9f0e14b061de547679cf
snaloz <bluemusicmonster@gmail.com>
2018-09-11 12:20:55 +0200

    dodanie szablonu Phyton

---
+++ pyhton/szablon.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szablon.py 
+
+
+def main(args):
+    return 0
+    print("Witaj phyton!")
+    
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
