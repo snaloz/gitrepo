e89346faa87f4577558eafae2384394680b4bdd0
snaloz <bluemusicmonster@gmail.com>
2018-10-23 12:36:31 +0200

    liczby 2 cpp

---
+++ cpp/liczb23.cpp
@@ -0,0 +1,29 @@
+/*
+ * liczb23.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int liczby2() {
+    int ile = 0;
+    for (int i = 1; i < 10; i++) {
+        for (int j = 1; j < 10; j++) {
+            if (i != j) {
+            cout << i << j << " ";
+            ile++;
+            }
+        }
+    }
+    return ile;
+}
+
+int main(int argc, char **argv)
+{
+    cout << "Liczb 2-cyfrowych: " << liczby2()
+    
+	;return 0;
+}
+
