73bfd9d2e956f979982e9980336a9aca7e133004
snaloz <bluemusicmonster@gmail.com>
2018-10-25 07:56:45 +0200

    nwd optymalny

--- cpp/euklides/main.cpp
+++ cpp/euklides/main.cpp
@@ -2,14 +2,29 @@
 
 using namespace std;
 int nwd_klasyczna(int a, int b) {
+    int licznik = 0;
     while (a != b) {
         if (a > b)
             a = a - b;
-            else
+        else
             b = b - a;
-        }
+        licznik++;
+    }
+    cout << "Powtórzeń: " << licznik << endl;
     return a;
 }
+
+
+int nwd_optymalny(int a, int b) {
+    int licznik = 0;
+    while (a > 0) {
+            a = a % b;
+            b = b - a;
+    }
+    cout << "Powtórzeń: " << licznik << endl;
+    return b;
+}
+
 
 int main()
 {
@@ -22,5 +37,7 @@ int main()
     cin >> b;
     cout << "NWD(" << a << " , " << b << ") = "
          << nwd_klasyczna(a, b) << endl;
+    cout << "NWD(" << a << " , " << b << ") = "
+         << nwd_optymalny(a, b) << endl;
     return 0;
 }
