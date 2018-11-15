c680afabb080cb6883ab0d1fac6f4fd66516f114
snaloz <bluemusicmonster@gmail.com>
2018-11-15 07:55:33 +0100

    palindromy

--- cpp/palindromy.cpp
+++ cpp/palindromy.cpp
@@ -3,20 +3,33 @@
  */
 
 #include <iostream>
+#include <string.h>
 
 using namespace std;
+
+bool palindrom(char w[], int r) {
+    bool czy_pal = true;
+    for (int i = 0; i < r/2; i++) {
+        if (w[i] != w[r - 1 - i]) {
+            czy_pal = false;
+            break;
+        }
+    }
+    return czy_pal;
+}
 
 int main(int argc, char **argv)
 {
 	const int rozmiar = 20;
-    char wyraz[20];
+    char wyraz[rozmiar];
     cout << "Podaj wyraz lub zdanie: ";
     cin.getline(wyraz, rozmiar);
     cout << cin.gcount() << endl;
     cout << strlen(wyraz) << endl;
     if (palindrom(wyraz, strlen(wyraz)))
         cout << "Palindrom!";
-    
+    else
+        cout << "Nie palindrom";
     return 0;
 }
 
