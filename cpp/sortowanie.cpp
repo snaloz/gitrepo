c4f68a4098428c17b608dece79a3d8ce53216f18
snaloz <bluemusicmonster@gmail.com>
2019-01-08 12:35:47 +0100

    sortowanie

--- cpp/sortowanie.cpp
+++ cpp/sortowanie.cpp
@@ -28,9 +28,22 @@ void zamien1(int a, int b) {
     int tmp = a;
     a = b;
     b = a;
-    cout << a << " " <, b << endl;
+    cout << a << " " < b << endl;
     
 }
+
+void sort_selection(int tab[], int n) {
+    int i, j, k;
+    for (i = 0; i < n; i++) {
+        k = 1;
+        for (j = i + 1; j < n; j++) {
+            if (tab[j] > tab[k])
+            k = j;
+            }
+            zamien1(tab[i], tab[i+1]);
+        }
+    }
+
 
 void sort_bubble(int tab[], int n) {
     for (int j = n-1; j > 0; j--) {
@@ -42,6 +55,19 @@ void sort_bubble(int tab[], int n) {
     }
 }
 
+void sort_insert(int tab[], int n) {
+    int i, k, el;
+    for (i = 1; i < n; i++) {
+        el = tab[i];
+        k = tab[i-1];
+        while (k>=0 && tab[k]>el) {
+            tab[k+1] = tab[k];
+            k--;
+            }
+            tab[k+1]=el;
+        }
+    }
+
 int main(int argc, char **argv)
 {
     int roz = 20;
@@ -49,7 +75,8 @@ int main(int argc, char **argv)
     wypelnij(tab, roz);
     drukuj (tab, roz);
     cout << endl << endl;
-    sort_bubble(tab, roz);
+    //sort_bubble(tab, roz);
+    sort_insert(tab, roz);
     cout << endl;
 	return 0;
 }
